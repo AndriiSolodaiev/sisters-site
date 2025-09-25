7bb68186f552ec9a7961cfd08a042cfcb0c153c9
yatsinoa <imsashasho@gmail.com>
2025-09-25 16:17:32 +0300

    ifame

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -936,3 +936,14 @@ document.addEventListener('DOMContentLoaded', function() {
     updateSelectedIds();
   });
 });
+
+if (document.querySelector('.section_view')) {
+  document.getElementById('loadIframeButton').addEventListener('click', function() {
+    var iframe = document.getElementById('lazyIframe');
+    iframe.src = iframe.getAttribute('data-src');
+    iframe.style.display = 'block';
+
+    this.style.display = 'none';
+    document.querySelector('.iframe-container').classList.add('active-iframe');
+  });
+}
