461fc25a660bc7660642f9fbd12dd42867c50eda
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-11-14 16:40:52 +0200

    feat: add animation for adv cards, change braikpoint for desktop view

--- src/assets/scripts/gulp-modules/new_page.js
+++ src/assets/scripts/gulp-modules/new_page.js
@@ -20,3 +20,22 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.advantages-title, .advantages-item ').forEach(block => {
+    gsap.fromTo(
+      block,
+      { y: 100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: block,
+          start: 'top 80%',
+          toggleActions: 'play none none none',
+        },
+      },
+    );
+  });
+});
